c025f4e950b032b76d0347df09e7e1bd73f78d2c
Brytni Spiers <brytni.spiers@gmail.com>
2021-09-08 18:52:02 -0300

    Rename stuff

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject org.clojars.brytnispiers/pedestal "0.1.0"
   :description "Wraps a Pedestal server as a component"
-  :url "https://github.com/brytnispiers93/pedestal-component.git"
+  :url "https://github.com/brytnispiers93/pedestal.git"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure          "1.10.0"]
@@ -8,4 +8,4 @@
                  [io.pedestal/pedestal.route   "0.5.7"]
                  [io.pedestal/pedestal.jetty   "0.5.7"]
                  [com.stuartsierra/component   "0.4.0"]]
-  :repl-options {:init-ns pedestal-component.core})
+  :repl-options {:init-ns pedestal})
